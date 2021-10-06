5f4e03b53bf64e92847b619eda2b2be3fcad3b6f
Mayllon Veras <mvs@cesar.school>
2021-10-05 21:11:48 -0300

    Adicionado consumo de endpoint: Informes

---
+++ src/Informes/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+
+
+class Informe extends Component{
+    render(){
+        return(
+            <View style={styles.item}>
+                <Text style={styles.title}>
+                    {this.props.data.txt_informe}
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default Informe;
+
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: '#3EB489',
+      padding: 15,
+      marginVertical: 3,
+      marginHorizontal: 10,
+    },
+    title: {
+      fontSize: 18,
+    },
+  });
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'http://192.168.0.114/psiu-gestor/public/api/'
+})
+
+export default api
