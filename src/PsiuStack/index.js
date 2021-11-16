c7e5066378a27d5075610b13f8adde680cfacead
Mayllon Veras <mvs@cesar.school>
2021-11-16 13:51:21 -0300

    Alterada barra superior

--- src/PsiuStack/index.js
+++ src/PsiuStack/index.js
@@ -14,13 +14,13 @@ const Stack = createStackNavigator();
 export default function PsiuStack() {
   return (
     <Stack.Navigator initialRouteName="Consentimento">
-      <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'PSIU' }}/>
-      <Stack.Screen name="Consentimento" component={Consentimento} options={{ title: 'Termo de consentimento' }}/>
-      <Stack.Screen name="Cadastro" component={Morador} />
-      <Stack.Screen name="Proposta" component={Proposta} />
-      <Stack.Screen name="Informes" component={Informes} />
-      <Stack.Screen name="Andamento" component={Andamento} options={{ title: 'Andamento das obras' }}/>
-      <Stack.Screen name="TabuleiroMedio" component={TabuleiroMedio} options={{ title: 'Tabuleiro Médio' }}/>
+      <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'PSIU', headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
+      <Stack.Screen name="Consentimento" component={Consentimento} options={{ title: 'Termo de consentimento', headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
+      <Stack.Screen name="Cadastro" component={Morador} options={{headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}} />
+      <Stack.Screen name="Proposta" component={Proposta} options={{headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
+      <Stack.Screen name="Informes" component={Informes} options={{headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
+      <Stack.Screen name="Andamento" component={Andamento} options={{ title: 'Andamento das obras', headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
+      <Stack.Screen name="TabuleiroMedio" component={TabuleiroMedio} options={{ title: 'Tabuleiro Médio', headerStyle: {backgroundColor: '#4B615B',}, headerTintColor: "#FFFFFF"}}/>
     </Stack.Navigator>
   );
 }
